6851343bf1ee6e16e01540ebc739795d2f5fcc7b
Daeyoung Kim <dkim@cellinobio.com>
2025-04-05 14:05:16 -0400

    Update host address

--- server.py
+++ server.py
@@ -47,4 +47,4 @@ async def get_stock_price(symbol: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
